dc2aae9c2945a12028612e66cf4b20003d4a55dc
Enrique <115951243+PEVF@users.noreply.github.com>
2023-08-29 15:47:41 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+    //Operaciones matemáticas
+  $('#ejecutar').click(function(e) {
+      e.preventDefault();
+      //Almaceno los valores de los inputs
+      var primerValor = $('#n1').val();
+      var segundoValor = $('#n2').val()
+
+      //Almacena el valor de la opción seleccionada
+      var opcionSeleccionada=document.getElementsByTagName("option");
+
+      //Condiciona para que acepte solo números usando las expresiones regulares
+      if(primerValor.match(/^[0-9]+$/) && segundoValor.match(/^[0-9]+$/)){
+          //Suma
+          if(opcionSeleccionada == 'suma'){
+              var resultado = parseFloat(primerValor) + parseFloat(segundoValor);
+          //Resta
+          }else if(opcionSeleccionada == 'resta'){ 
+              var resultado = parseFloat(primerValor) - parseFloat(segundoValor);
+
+          }else if(opcionSeleccionada == 'multiplicacion'){
+              var resultado = parseFloat(primerValor) * parseFloat(segundoValor);
+
+          }else {
+              var resultado = parseFloat(primerValor) + parseFloat(segundoValor);
+          }
+
+      }else{
+          alert("Ingrese números en los campos");
+      }
+      //Muestro el resultado
+    $('#resultado').text(resultado);
+  });
+});
